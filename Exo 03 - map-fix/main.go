6c69066a54ef462cc8e15959fc192fadf08bcaf8
saneto <aymanezaircha@gmail.com>
2019-10-04 18:30:42 +0200

    exo terminer

--- Exo 03 - map-fix/main.go
+++ Exo 03 - map-fix/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import ("fmt"
+	"errors"
+)
 
 type User struct {
 	FirstName, LastName string
@@ -17,10 +19,10 @@ func main() {
 	// TODO: fixme !
 	userList["DA92E99D-0C00-49E6-9343-FA89DA0670C6"] = u
 	// TODO: implement the error check and log it.
-	AddUser("DA92E99D-0C00-49E6-9343-FA89DA0670C6", u)
+	fmt.Println(AddUser("DA92E99D-0C00-49E6-9343-FA89DA0670C6", u))
 
 	// TODO: implement the error check and log it.
-	DeleteUser("63FC8E21-240A-4E75-98A2-962A5ED67177")
+	fmt.Println(DeleteUser("63FC8E21-240A-4E75-98A2-962A5ED67177"))
 
 	// this should display the user list
 	fmt.Println(userList)
@@ -29,21 +31,28 @@ func main() {
 // AddUser is adding a new user in the userList.
 // this send an error if the user allready exsits in the userList.
 func AddUser(uuid string, u User) error {
-	userList[uuid] = u
-	return nil
 	// TODO: implement this function
 	// this should add a new user in the userList map
 	// if the given uuid allready exists in the userList key this should send an error.
 	// otherwise this should send a nil error value
+	if _, ok := userList[uuid]; ok{
+		return errors.New("Uuid allready used")
+	}
+	userList[uuid] = u
+	return nil
 }
 
 // DeleteUser is deleting the given uuid user from the userList.
 // if the user don't exists in the userList this function sends an error.
 func DeleteUser(uuid string) error {
-	delete(userList,uuid)
-	return nil
+	
 	// TODO: implement this function.
 	// this should delete in the userList the given uuid.
 	// if the given uuid is not found this should send an error
 	// otherwise this should send a simple nil error value.
+	if _, ok := userList[uuid]; ok{
+		delete(userList,uuid)
+		return nil
+	}
+	return errors.New("Uuid not found")
 }
